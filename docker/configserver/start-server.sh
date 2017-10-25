f0c5722208bbe26a7d151e5b9818f78988003f12
bangnl <luongbangvh@gmail.com>
2017-10-25 20:16:16 +0700

    ađd git server docker

--- docker/configserver/start-server.sh
+++ docker/configserver/start-server.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 BASEDIR=$(dirname "$0")
+cd ~
+while (! nc -z gitserver 22); do
+    echo "Waiting for git is stared"
+    sleep 2
+done
+git clone git@gitserver:/srv/project.git
+mv application.properties project
+cd project
+git add .
+git commit -m "initial configuration"
+git push -u orgin master
 java -Djava.security.egd=file:/dev/./urandom -jar ${BASEDIR}/config-server.jar
