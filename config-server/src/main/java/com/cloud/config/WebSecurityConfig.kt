85f6f6b8492a0ccf996756746bb4cf3c89a64b06
bang nguyen luong <luongbangvh@gmail.com>
2017-11-15 11:21:05 +0700

    Fix docker for config cloud server

---
+++ config-server/src/main/java/com/cloud/config/WebSecurityConfig.kt
@@ -0,0 +1,34 @@
+package com.cloud.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+/**
+ * Created by nlbang on 11/14/2017.
+ */
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+    @Value("\${application.username}")
+    private lateinit var username: String
+
+    @Value("\${application.password}")
+    private lateinit var password: String
+
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests().antMatchers("/**").authenticated().and().httpBasic()
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        val userDetail = User.withDefaultPasswordEncoder().username(username).password(password).roles("USER").build()
+        auth.inMemoryAuthentication().withUser(userDetail)
+    }
+}
