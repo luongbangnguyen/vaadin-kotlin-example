018312bf0c31018a9e1b0163030cf2aabc5914e9
bang nguyen luong <luongbangvh@gmail.com>
2017-11-15 11:50:14 +0700

    check file application configuration existed

--- docker/configserver/start-server.sh
+++ docker/configserver/start-server.sh
@@ -4,13 +4,17 @@ while (! nc -z gitserver 22); do
     echo "Waiting for git is stared"
     sleep 2
 done
-ssh-keyscan -H gitserver >> ~/.ssh/known_hosts
-git config --global user.email "luongbangvh@gmail.com"
-git config --global user.name "Nguyen Luong Bang"
-git clone git@gitserver:/srv/project.git
-mv application.properties project
-cd project
-git add .
-git commit -m "initial configuration"
-git push -u origin master
+if [!- f application.properties]; then
+    echo "application.properties not found"
+else
+  ssh-keyscan -H gitserver >> ~/.ssh/known_hosts
+  git config --global user.email "luongbangvh@gmail.com"
+  git config --global user.name "Nguyen Luong Bang"
+  git clone git@gitserver:/srv/project.git
+  mv application.properties project
+  cd project
+  git add .
+  git commit -m "initial configuration"
+  git push -u origin master
+fi
 java -Djava.security.egd=file:/dev/./urandom -jar ${BASEDIR}/config-server.jar
